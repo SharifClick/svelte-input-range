e984ec5ef1dec06d8c479b2e040b23b54fb04451
Sharif Ahmed <me.sharifahmed@gmail.com>
2020-04-13 18:30:38 +0600

    fix visual

--- dev/App.svelte
+++ dev/App.svelte
@@ -1,18 +1,40 @@
 <script>
   import { InputRange } from "../src/index.js"
 
+  let binded_value = 0;
+
 </script>
 
 <style>
   .container{
+    display: flex;
+    flex-grow: 1;
     height: 100%;
     width: 100%;
     padding: 2rem;
   }
+
+  .column {
+    display: block;
+    flex-basis: 0;
+    flex-grow: 1;
+    flex-shrink: 1;
+    padding: 0.75rem;
+  }
+
+  .is-pmless{
+    padding: 0;
+    margin:0;
+  }
+
+  
 </style>
 <div class="container">
- <div style="width:33%">
-    <InputRange />
+ <div class="column">
+    <InputRange bind:value={binded_value}/>
+ </div>
+ <div class="column">
+   <h1 class="is-pmless">{binded_value}</h1>
  </div>
 </div>
 
