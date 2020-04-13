9805a46669d40806bcd2cfea06703bd01c2f9ee9
Sharif Ahmed <me.sharifahmed@gmail.com>
2020-04-13 17:51:13 +0600

    logics WIP

---
+++ src/InputRange.svelte
@@ -0,0 +1,69 @@
+<script>
+  let min =  0;
+  let max =  100;
+  let value =  50;
+  let classes = '';
+  let computedStyle = '';
+
+  $: {
+      let range = max - min;
+      let position = ((value - min) / range) * 100;
+      let positionOffset = Math.round(position * 0.40);
+      computedStyle = `left:calc(${position}% - ${positionOffset}px`;
+        
+  }
+  
+</script>
+
+<style>
+  .range-control {
+    position: relative;
+  }
+
+  .range-control > input[type="range"]::-webkit-slider-runnable-track {
+    background: lightgrey;
+    height: 0.357rem;
+    border-radius: 0.714rem;
+  }
+
+  .range-control > input[type="range"]::-moz-range-track {
+    background: lightgrey;
+    height: 5px;
+  }
+
+  .range-control > input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 2.857rem;
+    height: 1.714rem;
+    background:transparent;
+    margin-top: -0.714rem;
+    cursor: pointer;
+  }
+
+  .range-control > input[type="range"]::-moz-range-thumb {
+    width: 2.857rem;
+    height: 1.714rem;
+    background:transparent;
+    margin-top: -0.714rem;
+    cursor: pointer;
+  }
+  .range-control > output {
+    position: absolute;
+    top: 0.357em;
+    pointer-events: none;
+    display: block;
+    width: 2.857em;
+    height: 1.714em;
+    border: 1px solid #e2e2e2;
+    background-color: teal;
+    border-radius: 3px;
+    color: white;
+    font-size: 1rem;
+    line-height: 1.571em;
+    text-align: center;
+  }
+</style>
+<div class="range-control">
+  <input class="input {classes}" type="range" bind:value on:change {min} {max} />
+  <output style="{computedStyle}">{value}</output>
+</div>
